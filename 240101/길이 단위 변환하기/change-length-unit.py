9f759eeed82b94bf62c36b71775f86e964d6e1ed
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-01 21:55:22 +0900

    Title: 길이 단위 변환하기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/길이 단위 변환하기/change-length-unit.py
+++ 240101/길이 단위 변환하기/change-length-unit.py
@@ -6,3 +6,6 @@ ans_mile = 1.3 * 160934
 
 print(f"9.2ft = {ans_feet:0.1f}cm")
 print(f"1.3mi = {ans_mile:0.1f}cm")
+
+# a, b = 9.2, 1.3
+# print(f"{a:0.1f}ft = {a*30.48:0.4f}cm\n{b:0.1f}mi = {b*160934:0.1f}cm})
