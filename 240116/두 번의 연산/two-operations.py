de4247bc65c6cafaab0643b8d05a3e7859228fc9
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-16 10:37:44 +0900

    Title: 두 번의 연산, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/두 번의 연산/two-operations.py
+++ 240116/두 번의 연산/two-operations.py
@@ -5,7 +5,12 @@ if a % 2 != 0:
     if a % 3 == 0:
         a /= 3
         print(int(a))
+    else:
+        print(int(a))
 
 elif a % 3 == 0:
     a /= 3
+    print(int(a))
+
+else:
     print(int(a))
