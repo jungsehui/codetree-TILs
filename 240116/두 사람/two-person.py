0faf8ea3ba314107417a2b6cb684bda2da7083a4
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-16 16:04:21 +0900

    Title: 두 사람, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/두 사람/two-person.py
+++ 240116/두 사람/two-person.py
@@ -1,9 +1,9 @@
 age1, gender1 = input().split()
 age2, gender2 = input().split()
 
-if age1 >= 19 and gender1 == 'M':
+if int(age1) >= 19 and gender1 == 'M':
     print("1")
-elif age2 >= 19 and gender2 == 'M':
+elif int(age2) >= 19 and gender2 == 'M':
     print("1")
 else:
     print("0")
