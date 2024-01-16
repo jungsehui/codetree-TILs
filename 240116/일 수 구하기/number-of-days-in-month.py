236ab1a788041c786469b06085be4e7c597521a0
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-16 17:20:07 +0900

    Title: 일 수 구하기, Time: 192ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240116/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+if n <= 7:
+    if n % 2 != 0:  # 홀수 월일 경우
+        print(31)
+    elif n == 2:
+        print(28)
+    else:
+        print(30)
+
+else:
+    if n % 2 != 0:
+        print(30)
+    else:
+        print(31)
