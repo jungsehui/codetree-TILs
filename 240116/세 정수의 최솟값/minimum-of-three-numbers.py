e3a808414938063a466c99c0ef37f8f9dd5ce65e
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-16 14:02:00 +0900

    Title: 세 정수의 최솟값, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240116/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -1,8 +1,4 @@
 a, b, c = map(int, input().split())
 
-arr = [a, b, c]
-lowest = min(arr)
-
-result = [num for num in arr if num == lowest]
-
-print(*result[0])
+result = min(a, b, c)
+print(result)
