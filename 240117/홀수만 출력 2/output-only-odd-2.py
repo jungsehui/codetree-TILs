b8dd7ec99b7640a755606e00a71258de1efe739d
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-17 14:33:54 +0900

    Title: 홀수만 출력 2, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240117/홀수만 출력 2/output-only-odd-2.py
+++ 240117/홀수만 출력 2/output-only-odd-2.py
@@ -1,8 +1,5 @@
 a, b = map(int, input().split())
-arr = list()
 
 for i in range(b, a-1, -1):
-    n = i if i % 2 != 0
-    arr.append(n)
-
-print(*arr)
+    if i % 2 != 0:
+        print(i, end=' ')
