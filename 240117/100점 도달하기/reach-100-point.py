1a9e887ca389bd86b7980f343ea368904f1263b6
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-17 17:14:02 +0900

    Title: 100점 도달하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240117/100점 도달하기/reach-100-point.py
+++ 240117/100점 도달하기/reach-100-point.py
@@ -1,17 +1,17 @@
 n = int(input())
 
 if n >= 90:
-    for i in range(n, 100+1)
+    for i in range(n, 100+1):
         print("A")
 elif n >= 80:
-    for i in range(n, 100+1)
+    for i in range(n, 100+1):
         print("B")
 elif n >= 70:
-    for i in range(n, 100+1)
+    for i in range(n, 100+1):
         print("C")
 elif n >= 60:
-    for i in range(n, 100+1)
+    for i in range(n, 100+1):
         print("C")
 else:
-    for i in range(n, 100+1)
+    for i in range(n, 100+1):
         print("F")
