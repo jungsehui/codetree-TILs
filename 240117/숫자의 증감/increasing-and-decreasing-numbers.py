2702dc9906a310cf40c9f44dc39197d3c0f425fd
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-17 17:03:56 +0900

    Title: 숫자의 증감, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240117/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,8 @@
+c, n = input().split()
+
+if c == 'A':
+    for i in range(1, int(n) + 1):
+        print(i, end=' ')
+elif c == 'D':
+    for j in range(int(n), 1 - 0, -1):
+        print(j, end=' ')
