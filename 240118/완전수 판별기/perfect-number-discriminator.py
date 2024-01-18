a0bafceee1116e6a6a3713117e2e3258aad86ed4
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-18 14:06:42 +0900

    Title: 완전수 판별기, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240118/완전수 판별기/perfect-number-discriminator.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = []
+
+for i in range(1, n+1):
+    if n % i == 0:
+        arr.append(i)
+
+t = 0
+for j in arr:
+    t += j
+
+if t == n:
+    print("P")
+else:
+    print("N")
