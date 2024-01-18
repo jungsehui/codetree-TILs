83e1caf25197097cb34fae07184dadc0aad508e7
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-18 10:03:16 +0900

    Title: 청소당번 정하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/청소당번 정하기/cleaning-numbering.py
+++ 240118/청소당번 정하기/cleaning-numbering.py
@@ -4,12 +4,12 @@ cnt2 = 0
 cnt3 = 0
 cnt12 = 0
 
-for _ in range(n):
-    if n % 12 == 0:
+for i in range(1, n+1):
+    if i % 12 == 0:
         cnt12 += 1
-    elif n % 3 == 0:
+    elif i % 3 == 0:
         cnt3 += 1
-    elif n % 2 == 0:
+    elif i % 2 == 0:
         cnt2 += 1
 
 print(cnt2, cnt3, cnt12)
