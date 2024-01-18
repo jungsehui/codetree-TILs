bcac54229885db13fab60e741dc847db594a3d97
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-18 15:10:55 +0900

    Title: 친근하지 않은 수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/친근하지 않은 수/unfriendly-number.py
+++ 240118/친근하지 않은 수/unfriendly-number.py
@@ -1,7 +1,9 @@
 n = int(input())
 cnt = 0
 for i in range(1, n+1):
-    if not (i % 2 == 0 and i % 3 == 0 and i % 5 == 0):
+    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
+        continue
+    else:
         cnt += 1
 
 print(cnt)
