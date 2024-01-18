430d0357c694bac6c53ed8540587dbc7dfabe9f7
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-18 15:24:14 +0900

    Title: 특정 조건의 온전수 구하기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 240118/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,5 +1,11 @@
 n = int(input())
 
-for num in range(1, n+1):
-    if num % 2 != 0 and (num % 5 != 0 or num % 10 == 0) and (num % 3 != 0 or num % 9 == 0):
-        print(num, end=" ")
+for num in range(1, n + 1):
+    if num % 2 == 0:
+        continue
+    if num % 10 == 5:
+        continue
+    if num % 3 == 0 and num % 9 != 0:
+        continue
+
+    print(num, end=" ")
