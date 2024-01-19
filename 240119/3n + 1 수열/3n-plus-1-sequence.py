c2cf4cdfbe554b48fd28f1d1eb69b796f9ab680b
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-19 10:48:46 +0900

    Title: 3n + 1 수열, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/3n + 1 수열/3n-plus-1-sequence.py
+++ 240119/3n + 1 수열/3n-plus-1-sequence.py
@@ -2,6 +2,9 @@ n = int(input())
 cnt = 0
 
 while True:
+    if n == 1 :
+        break
+
     if n % 2 == 0:
         n /= 2
         cnt += 1
@@ -10,8 +13,5 @@ while True:
         n *= 3
         n += 1
         cnt += 1
-    
-    if n == 1 :
-        break
 
 print(cnt)
