d6a20d063093eda17d721d2adb00fe19cdcc5d13
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-23 17:08:32 +0900

    Title: 체크 다이아몬드, Time: 179ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240123/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range(n - 1 - i):
+        print(" ", end="")
+    for _ in range(1 + i):
+        print("*", end=" ")
+    print()
+
+for i in range(n - 1):
+    for _ in range(1 + i):
+        print(" ", end="")
+    for _ in range(n - 1 - i):
+        print("*", end=" ")
+    print()
