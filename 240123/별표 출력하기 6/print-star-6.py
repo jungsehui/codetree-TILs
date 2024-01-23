5ba9a267c5f74ada211d3e44e5346d203b1ee853
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-23 14:05:02 +0900

    Title: 별표 출력하기 6, Time: 126ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240123/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    for _ in range(i):
+        print(" ", end=" ")
+    for _ in range((n * 2) - 1 - (i * 2)):
+        print("*", end=" ")
+    print()
+
+
+for i in range(n - 1):
+    for _ in range(n - i - 2):
+        print(" ", end=" ")
+    for _ in range(3 + (2 * i)):
+        print("*", end=" ")
+    print()
