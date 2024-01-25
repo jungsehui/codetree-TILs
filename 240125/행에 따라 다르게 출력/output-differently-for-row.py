d1c582ba46ed571b8c0188da7924fded6929426b
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-25 14:22:25 +0900

    Title: 행에 따라 다르게 출력, Time: 149ms, Memory: 29MB, Status: Passed - CodeTree

--- 240125/행에 따라 다르게 출력/output-differently-for-row.py
+++ 240125/행에 따라 다르게 출력/output-differently-for-row.py
@@ -1,20 +1,17 @@
 n = int(input())
-cnt = 1
-even_line_number = 0
+cnt1 = 1
+cnt2 = 2
+end_number = 0
 
 for i in range(n):
     for j in range(n):
-        if i == 0:
-            print(1 + j, end=" ")
-        elif i % 2 == 0:  # 홀수 번째 줄 : 1씩 증가
-            # print((1 + j) + (n * (1 + i)), end=" ")
-            even_line_number += 1
-            print(even_line_number, end=" ")
-        else:  # 짝수 번째 줄 : 2씩 증가
-            print(((n + 2) * i) + (2 * j), end=" ")
-            even_line_number = ((n + 2) * i) + (2 * j)
+        if i % 2 == 0:
+            print(end_number + cnt1, end=" ")
+            end_number += cnt1
+        else:
+            print(end_number + cnt2, end=" ")
+            end_number += cnt2
     print()
-
 
 # if i == 0:
 #     print(1 + j, end=" ")
