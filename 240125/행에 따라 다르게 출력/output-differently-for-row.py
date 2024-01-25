b32c49095ee878e35995674c8924d69f475903a7
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-25 14:13:16 +0900

    Title: 행에 따라 다르게 출력, Time: 127ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240125/행에 따라 다르게 출력/output-differently-for-row.py
+++ 240125/행에 따라 다르게 출력/output-differently-for-row.py
@@ -1,11 +1,20 @@
 n = int(input())
+cnt = 1
 
 for i in range(n):
     for j in range(n):
         if i == 0:
-            print(1 + j, end=" ")
+            print(cnt, end=" ")
+            cnt += 1
         elif i % 2 == 0:  # 홀수 번째 줄 : 1씩 증가
-            print((1 + j) + (n * (1 + i)) , end=" ")
-        else:           # 짝수 번째 줄 : 2씩 증가
+            print((1 + j) + (n * (1 + i)), end=" ")
+        else:  # 짝수 번째 줄 : 2씩 증가
             print(((n + 2) * i) + (2 * j), end=" ")
     print()
+
+# if i == 0:
+#     print(1 + j, end=" ")
+# elif i % 2 == 0:  # 홀수 번째 줄 : 1씩 증가
+#     print((1 + j) + (n * (1 + i)), end=" ")
+# else:  # 짝수 번째 줄 : 2씩 증가
+#     print(((n + 2) * i) + (2 * j), end=" ")
