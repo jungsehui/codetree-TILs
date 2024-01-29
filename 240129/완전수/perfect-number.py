9901f2fc2b0194fc22428583938c233719f664c5
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-29 12:30:06 +0900

    Title: 완전수, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240129/완전수/perfect-number.py
+++ 240129/완전수/perfect-number.py
@@ -1,21 +1,13 @@
-def find_divisors(n):
-    divisors = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
+start, end = map(int, input().split())
+cnt = 0
 
-def main():
-    start, end = map(int, input().split())
-    cnt = 0
+for i in range(start, end + 1):
+    sumN = 0
+    for j in range(1, i):
+        if i % j == 0:
+            sumN += j
 
-    for i in range(start, end + 1):
-        divisors = find_divisors(i)
-        sumN = sum(divisors) - i  # 자기 자신을 빼줌
-        if sumN == i:
-            cnt += 1
+    if sumN == i:
+        cnt += 1
 
-    print(cnt)
-
-if __name__ == "__main__":
-    main()
+print(cnt)
