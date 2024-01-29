a1380aedbba784d830e7282908ba8b4a697cf1cd
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-29 17:19:23 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240129/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+stu_cnt = 0
+
+for _ in range(n):
+    arr = list(map(int, input().split()))
+    s = 0
+    cnt = 0
+    avr = 0
+
+    for elem in arr:
+        s += elem
+        cnt += 1
+
+    avr = s / cnt
+
+    if avr >= 60:
+        print("pass")
+    else:
+        print("fail")
