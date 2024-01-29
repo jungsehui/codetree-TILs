8fa0e252c8f1822fbd8690d43d71f8b597bada50
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-29 14:40:08 +0900

    Title: 소수 구하기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/소수 구하기/get-prime.py
+++ 240129/소수 구하기/get-prime.py
@@ -1,29 +1,14 @@
-# n = int(input())
-# prime_num_list = []
-#
-#
-# for i in range(2, n + 1):
-#     for k in range(2, int(i ** 0.5) + 1):
-#         if i % k == 0:
-#             break
-#
-#         if i == k:
-#             prime_num_list.append(i)
-#
-# print(*prime_num_list)
-
 n = int(input())
 prime_num_list = []
+
 
 for i in range(2, n + 1):
     is_prime = True
 
-    for k in range(2, int(i ** 0.5) + 1):
+    for k in range(2, i):
         if i % k == 0:
             is_prime = False
             break
 
     if is_prime:
-        prime_num_list.append(i)
-
-print(*prime_num_list)
+        print(i, end=" ")
