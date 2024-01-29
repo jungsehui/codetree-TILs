1df294bd80ef6a01c9b0626b5b88ed1e6e3e4a52
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-29 14:20:06 +0900

    Title: 소수 구하기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/소수 구하기/get-prime.py
+++ 240129/소수 구하기/get-prime.py
@@ -1,13 +1,29 @@
-i = 0
+# n = int(input())
+# prime_num_list = []
+#
+#
+# for i in range(2, n + 1):
+#     for k in range(2, int(i ** 0.5) + 1):
+#         if i % k == 0:
+#             break
+#
+#         if i == k:
+#             prime_num_list.append(i)
+#
+# print(*prime_num_list)
+
 n = int(input())
-count = 0
+prime_num_list = []
 
-for k in range(1, n + 1):
-    for i in range(2, k + 1, 1):
-        if k % i == 0:  # 나누어 떨어지는 수는 소수가 아님
-            break  # for문 다시 시작
+for i in range(2, n + 1):
+    is_prime = True
 
-    if k == i:  # k(입력받은 n1부터 n2까지 변수)를 i(2부터 시작)로 나누다가
-        count += 1  # 나눠지는 수가 없어서 i가 k까지 가면 1과 k만 있으니 소수
+    for k in range(2, int(i ** 0.5) + 1):
+        if i % k == 0:
+            is_prime = False
+            break
 
-        print(k, end=" ")  # 출력
+    if is_prime:
+        prime_num_list.append(i)
+
+print(*prime_num_list)
