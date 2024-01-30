dfe473aff66937958d991775f085b53879f248df
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-30 20:40:10 +0900

    Title: 십의 자리 숫자, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/십의 자리 숫자/number-of-tens-digit.py
+++ 240130/십의 자리 숫자/number-of-tens-digit.py
@@ -13,3 +13,30 @@ for index in range(len(cnt_arr)):
 
 for index, num in enumerate(cnt_arr):
     print(f"{index + 1} - {num}")
+
+
+
+# 배열에서 0만 빼고 남김
+# arr = list(map(int, input().split()))
+# cnt_arr = [0] * 9
+
+# for index, num in enumerate(arr):
+#     if num == 0:
+#         arr.remove(arr[index])
+#         break
+
+# print(len(arr))
+# print(arr)
+
+# cnt = 0
+
+# for index in range(len(cnt_arr)):
+#     for number in arr:
+#         if index + 1 == number // 10:
+#             cnt_arr[index] += 1
+#             cnt += 1
+
+# for index, num in enumerate(cnt_arr):
+#     print(f"{index + 1} - {num}")
+
+# print(cnt)
