9969eeae1d993397343a5c3cd27e46e2c8d7d8ec
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-30 10:35:49 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240130/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+summary = 0
+
+for i in range(len(arr)):  # enum도 가능
+    if arr[i] == 0:
+        summary = arr[i - 3] + arr[i - 2] + arr[i - 1]
+    
+print(summary)
+
+# for index, elem in enumerate(arr):
+#     if elem == 0:
+#         summary = arr[index - 3] + arr[index - 2] + arr[index - 1]
+#     
+# print(summary)
