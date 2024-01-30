f592ac6b00f0eaf9d4bf6ee488f6721d2a52dae1
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-30 17:08:00 +0900

    Title: 전항의 두 배, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/전항의 두 배/twice-the-previous.py
+++ 240130/전항의 두 배/twice-the-previous.py
@@ -1,10 +1,8 @@
 a1, a2 = map(int, input().split())
 
 arr = [a1, a2]
-for i in range(2, 10):
-    arr.append(1)
 
 for i in range(2, 10):
-    arr[i] = arr[i - 1] + (arr[i - 2] * 2)
+    arr.append(arr[i - 1] + (arr[i - 2] * 2))
 
 print(*arr)
