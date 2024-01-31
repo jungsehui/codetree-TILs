c41128646a5ecf18248776cd37d66bab44ae0336
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-31 11:04:09 +0900

    Title: 나눗셈의 나머지, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/나눗셈의 나머지/remainder-of-division.py
+++ 240131/나눗셈의 나머지/remainder-of-division.py
@@ -1,17 +1,10 @@
-arr = [0] * 10
-
 a, b = map(int, input().split())
-
-while True:
-    mod = a % b
-    arr[mod] += 1
-
-    a //= b
-
-    if a <= 1:
-        break
-
+arr = [0] * 10
 mod_cnt = 0
+
+while a > 1:
+    arr[a % b] += 1
+    a //= b
 
 for num in arr:
     mod_cnt += num ** 2
