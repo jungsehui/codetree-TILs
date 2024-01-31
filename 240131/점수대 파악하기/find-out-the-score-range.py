e1262693eb9ac7faf6ed3cbfb1312db4a13497dd
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-01-31 10:14:15 +0900

    Title: 점수대 파악하기, Time: 153ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240131/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input().split()))
+cnt_arr = [0] * 10
+
+for index in range(len(arr)):
+    for elem in arr:
+        if elem // 10 == index + 1:
+            cnt_arr[index] += 1
+
+
+for index in range(len(cnt_arr), 0, -1):
+    print(f"{index * 10} - {cnt_arr[index - 1]}")
