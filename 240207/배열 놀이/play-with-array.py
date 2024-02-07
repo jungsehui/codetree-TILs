5970541ff404200568b604aa8a7d480ec75ca3f3
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-02-07 18:15:31 +0900

    Title: 배열 놀이, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/배열 놀이/play-with-array.py
+++ 240207/배열 놀이/play-with-array.py
@@ -1,30 +1,24 @@
 n, p = map(int, input().split())
-
 num_list = list(map(int, input().split()))
 
 for _ in range(p):
     tmp_list = list(map(int, input().split()))
-    if len(tmp_list) == 2:
-        prob_index,question = tmp_list
-    elif len(tmp_list) == 3:
-        prob_index,start_index,end_index = tmp_list
+
+    prob_index = tmp_list[0]
 
     if prob_index == 1:
+        question = tmp_list[1]
         print(num_list[question-1])
+
     elif prob_index == 2:
-        try:
-            print(num_list.index(question)+1)
-        except:
+        question = tmp_list[1]
+        found_indices = [i+1 for i, num in enumerate(num_list) if num == question]
+
+        if found_indices:
+            print(min(found_indices))
+        else:
             print(0)
-        # answer = -1
-        # for num_index,num in enumerate(num_list):
-        #     if num == question:
-        #         answer = num_index+1
-        #         break
-        # if answer == -1:
-        #     print(0)
-        # else:
-        #     print(answer)
+
     elif prob_index == 3:
-        tmp_num_list = list(map(str,num_list))
-        print(' '.join(tmp_num_list[start_index-1:end_index]))
+        start_index, end_index = tmp_list[1], tmp_list[2]
+        print(' '.join(map(str, num_list[start_index-1:end_index])))
