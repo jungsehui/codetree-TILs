de00d3463e198d2036ac8cdb970f86edeac53aa2
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-05-07 23:19:52 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240507/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,16 +1,42 @@
+# n = int(input())
+# n_list= list(map(int, input().split()))
+
+# max_number = 0
+# duplicated = []
+
+# for i in range(n):
+#     if n_list[i] in duplicated:
+#         continue
+
+#     if n_list[i] > max_number:
+#         max_number = n_list[i]
+
+#     if n_list[i] == max_number:
+#         duplicated.append(n_list[i])
+    
+#     if max_number in duplicated[i]:
+#         print(-1)
+    
+# print(max_number)
+
+
 n = int(input())
-n_list= list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-max_number = max(n_list)  # 리스트에서 최댓값을 찾음
-
-# 중복되지 않는 숫자 중에서 최댓값을 찾음
-for num in n_list:
-    if n_list.count(num) > 1:  # 현재 숫자가 중복되면 넘어감
+max_val = 0
+dup = []
+for i in range(n):
+    if arr[i] in dup:
         continue
-    if num > max_number:
-        max_number = num
+    
+    if arr[i] in arr[i + 1:]:
+        dup.append(arr[i])
+        continue
 
-if max_number in n_list:  # 최댓값이 리스트에 있으면 -1 출력
-    print(max_number)
+    if arr[i] > max_val:
+        max_val = arr[i]
+
+if max_val == 0:
+    print(-1)
 else:
-    print(-1)  # 최댓값 출력
+    print(max_val)
