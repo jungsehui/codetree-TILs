cbd6d5fcca42e1d5a31910df79f73f06fbe5e1cf
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-06-27 20:25:36 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 224ms, Memory: 13MB, Status: Passed - CodeTree

--- 240627/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240627/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -15,25 +15,26 @@ public class Main {
         }
 
         int maxVal = -1;
-        int count = 0;
 
         // 1 2 1 2
         for (int j = 0; j < N; j++) {
-            if (arr[j] > maxVal) {
-                maxVal = arr[j];
-            }
+            int currNum = arr[j];
+            
+            if (maxVal < currNum) {
+                int count = 0;
 
-            for (int k = 0; k < N; k++) {
-                if (maxVal == arr[k]) {
-                    count++;
+                for (int k = 0; k < N; k++) {
+                    if (currNum == arr[k]) {
+                        count++;
+                    }
+                }
+
+                if (count == 1) {
+                    maxVal = currNum;
                 }
             }
         }
 
-        if (count == 2) {
-            System.out.print(-1);
-        } else {
-            System.out.print(maxVal);
-        }
+        System.out.print(maxVal);
     }
 }
