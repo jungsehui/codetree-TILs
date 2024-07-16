9c7110ed751c148cf979014ed913bf85dba6265e
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-07-16 20:18:15 +0900

    Title: 증가시키며 출력하기 3, Time: 80ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240716/증가시키며 출력하기 3/increase-and-print-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 변수 선언
+        int i = 10;
+
+        // 출력
+        while(i <= 26) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
