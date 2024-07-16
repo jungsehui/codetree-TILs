9e86137891e623da2acb5005cd6e1a9a2778938d
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-07-16 20:20:27 +0900

    Title: 짝수만 출력, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240716/짝수만 출력/output-only-even.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int a, b;
+        int i;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        i = a;
+
+        // 출력
+        while(i <= b) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
