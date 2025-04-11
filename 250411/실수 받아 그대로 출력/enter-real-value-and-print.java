f062d91ad90968c042a36313dc23ea75cd0c2417
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-11 14:54:53 +0900

    Title: 실수 받아 그대로 출력, Time: 90ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250411/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        double a = Integer.parseInt(br.readLine());
+        System.out.printf("%.2f", a);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
