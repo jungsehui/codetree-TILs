94d5ebfb000f90d558631867126bbebe2c02324c
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-11 15:14:31 +0900

    Title: 입력받은 값과 합 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250411/입력받은 값과 합 출력/output-of-value-and-sum.java
+++ 250411/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -12,7 +12,7 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        bw.write(String.valueOf(a + " " + b + (a + b)));
+        bw.write(String.valueOf(a + " " + b + " " (a + b)));
         bw.flush();
         bw.close();
         br.close();
