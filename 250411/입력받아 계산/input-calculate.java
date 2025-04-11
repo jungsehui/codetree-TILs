44f57a0dd93e3f57ca17e0e66f4e40ddc9b7e508
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-11 14:45:41 +0900

    Title: 입력받아 계산, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250411/입력받아 계산/input-calculate.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int a = Integer.parseInt(br.readLine());
+        a += 2;
+        
+        bw.write(a);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
