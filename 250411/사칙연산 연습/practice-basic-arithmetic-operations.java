3cc580817f67aa103517ea746872c28166b802d2
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-11 13:59:44 +0900

    Title: 사칙연산 연습, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250411/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+        int quotient = c / 8;
+        int remainder = c % 8;
+        System.out.println(quotient);
+        System.out.println(remainder);
+    }
+}
