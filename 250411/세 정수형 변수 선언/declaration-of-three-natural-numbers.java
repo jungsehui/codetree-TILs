3d183177f2d80549e7cd9b425358b245655c83f7
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-11 14:26:59 +0900

    Title: 세 정수형 변수 선언, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250411/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 7;
+        int b = 23;
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum)
+    }
+}
