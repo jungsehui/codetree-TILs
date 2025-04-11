2f54b03b25db459d38fa13f7c7dae2ee23482092
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-12 00:21:29 +0900

    Title: 굉장한 수, Time: 83ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250412/굉장한 수/amazing-number.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int AM = Integer.parseInt(st.nextToken());
+//        int AE = Integer.parseInt(st.nextToken());
+//        st = new StringTokenizer(br.readLine());
+//        int BM = Integer.parseInt(st.nextToken());
+//        int BE = Integer.parseInt(st.nextToken());
+
+        if (AM % 2 != 0 && AM % 3 == 0) {
+            System.out.println("true");
+        } else if (AM % 5 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
