2af469548b7df6a19e88f085f0151a2014ff7368
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-12 00:36:12 +0900

    Title: 배열의 합 계산하기, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250412/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+//        st = new StringTokenizer(br.readLine());
+//        int AM = Integer.parseInt(st.nextToken());
+//        int BM = Integer.parseInt(st.nextToken());
+//        int CM = Integer.parseInt(st.nextToken());
+//        int AE = Integer.parseInt(st.nextToken());
+//        st = new StringTokenizer(br.readLine());
+//        int BM = Integer.parseInt(st.nextToken());
+//        int BE = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[] {3, 1, 4, 5, 9};
+//        for (int i = 0; i < 3; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//        Arrays.sort(arr);
+
+        System.out.println(arr[1] + arr[3] + arr[4]);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
