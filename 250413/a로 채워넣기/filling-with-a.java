244dd275b89d84e44a5ff37db68899f652cf943b
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-13 00:22:49 +0900

    Title: a로 채워넣기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250413/a로 채워넣기/filling-with-a.java
+++ 250413/a로 채워넣기/filling-with-a.java
@@ -2,30 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        StringBuilder a = new StringBuilder(st.nextToken());
-//        String b = st.nextToken();
-//        int f = Integer.parseInt(st.nextToken());
-//        int s = Integer.parseInt(st.nextToken());
-//
-//        for (int i = 0; i < 10; i++) {
-//            System.out.print((f % 10) + " ");
-//            int next = (f + s) % 10;
-//            f = s;
-//            s = next;
-//        }
-//        leebroscode
+        String a = st.nextToken();
 
-        a.setCharAt(1, 'a');
-        a.setCharAt(a.length() - 1, 'a');
+        StringBuilder sb = new StringBuilder(a);
+        if (sb.length() > 1) {
+            sb.setCharAt(1, 'a');
+        }
 
-        bw.write(a.toString());
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
