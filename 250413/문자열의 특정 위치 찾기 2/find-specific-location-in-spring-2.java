75844519b7720dd4b7350b6c565bfff88590d27d
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-13 21:50:51 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250413/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+
+        String[] strings = {"apple", "banana", "grape", "blueberry", "orange"};
+
+        int count = 0;
+        char c = st.nextToken().charAt(0);
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            if (string.charAt(2) == c || string.charAt(3) == c) {
+                sb.append(string).append(System.lineSeparator());
+                count++;
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static String check(int score) {
+        String ret = "F";
+        if (score >= 90) {
+            ret = "A";
+        } else if (score >= 80) {
+            ret = "B";
+        } else if (score >= 70) {
+            ret = "C";
+        } else if (score >= 60) {
+            ret = "D";
+        } else {
+            return "F";
+        }
+        return ret;
+    }
+}
