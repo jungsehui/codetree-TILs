254c6695c8fb2c60ab4d140f1f69eb33f3cffce9
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-14 22:14:27 +0900

    Title: 약수가 세 개인 수, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250414/약수가 세 개인 수/numbers-with-three-divisors.java
+++ 250414/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
+
     public static boolean isPrime(int n) {
         if (n < 2) return false;
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
         }
         return true;
@@ -13,18 +13,20 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        for (int i = N; i <= M; i++) {
-            int sqrt = (int) Math.sqrt(i);
+        for(int i = N; i <= M; i++){
+            int sqrt = (int)Math.sqrt(i);
             if (sqrt * sqrt == i && isPrime(sqrt)) {
-                System.out.println(i);
+                bw.write(i + "\n");
             }
         }
-
+        bw.flush();
+        bw.close();
         br.close();
     }
 }
