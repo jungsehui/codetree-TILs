969262d01c2c6c7773d3902f5a959b4d86ae66fd
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-14 22:09:55 +0900

    Title: 약수가 세 개인 수, Time: 101ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250414/약수가 세 개인 수/numbers-with-three-divisors.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        for(int i = N; i <= M; i++){
+            int count = 0;
+            for(int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 3) {
+                System.out.println(i);
+            }
+        }
+
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < M; j++) {
+//                sb.append("*").append(" ");
+//            }
+//            sb.append(System.lineSeparator());
+//        }
+//
+//        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static String check(int score) {
+        String ret = "F";
+        if (score >= 90) {
+            ret = "A";
+        } else if (score >= 80) {
+            ret = "B";
+        } else if (score >= 70) {
+            ret = "C";
+        } else if (score >= 60) {
+            ret = "D";
+        } else {
+            return "F";
+        }
+        return ret;
+    }
+
+    private static void temp() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+//        int N = Integer.parseInt(st.nextToken());
+//        int M = Integer.parseInt(st.nextToken());
+
+        String[] strings = {"apple", "banana", "grape", "blueberry", "orange"};
+
+        int count = 0;
+        char c = st.nextToken().charAt(0);
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            if (string.charAt(2) == c || string.charAt(3) == c) {
+                sb.append(string).append(System.lineSeparator());
+                count++;
+            }
+        }
+
+        br.close();
+    }
+}
