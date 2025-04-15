3284db724013067e340f4ce2c82531c60760bf7e
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-16 05:38:19 +0900

    Title: 상하로 반복하며 출력, Time: 86ms, Memory: 9MB, Status: Runtime Error - Codetree

---
+++ 250416/상하로 반복하며 출력/print-in-up-down-reverse.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][N];
+        int top = 0;
+        int bottom = N - 1;
+        int left = 0;
+        int right = N - 1;
+        int max = N * N;
+        int num = 1;
+        
+        while (left < N) {
+            for (int i = top; i <= bottom; i++) {
+                arr[i][left] = i + 1;
+            }
+            left++;
+            
+            for (int i = bottom; i >= top; i--) {
+                arr[i][left] = N - i;
+            }
+            left++;
+        }
+
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt);
+            }
+            System.out.println();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
