7949c3c3147dcd501d1f8293df5d4c458a81dff1
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-16 03:41:52 +0900

    Title: 특정 원소들의 합, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250416/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int[][] num1 = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 4; j++) {
+                num1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                count += num1[i][j];
+            }
+        }
+
+        System.out.println(count);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
