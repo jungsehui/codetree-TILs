bdbe6958056fbe4e21b37354314cab3493b97c71
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-17 01:47:55 +0900

    Title: 격자로 사각형 만들기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250417/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][N];
+        int top = 0;
+        int bottom = 4;
+        int left = 0;
+        int right = 4;
+        int num = 2;
+
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = 1;
+            arr[0][i] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                bw.write(arr[i][j] + " ");
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
