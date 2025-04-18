768571ef479177f0a1161665571713a1ef5983cf
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-18 21:39:19 +0900

    Title: 문자열의 길이 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250418/문자열의 길이 출력하기/print-strings-length.java
+++ 250418/문자열의 길이 출력하기/print-strings-length.java
@@ -12,9 +12,11 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine());
-        String s = st.nextToken();
+        String s1 = st.nextToken();
+        String s2 = st.nextToken();
 
-        System.out.println(s.length());
+        System.out.println(s1.length() + s2.length)());
+
         bw.flush();
         bw.close();
         br.close();
