ee1fc4edcefa5fbf874571e4f15c3b331f8d96d0
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-04-20 19:09:42 +0900

    Title: 공백을 기준으로 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250420/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            sb.append(s);
+        }
+
+        bw.wrtie(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
