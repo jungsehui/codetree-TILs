746ec40787f48c14c49363d1c1d5b86b7f727d66
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-10-28 22:26:54 +0900

    Title: 특정 문자의 등장 횟수, Time: 82ms, Memory: 9MB, Status: Passed - Codetree

--- 251028/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
+++ 251028/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -1,7 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // Please write your code here.
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+
+        int eeCount = 0;
+        int ebCount = 0;
+
+        for (int i = 0; i < line.length() - 1; i++) {
+            String sub = line.substring(i, i + 2);
+
+            if (sub.equals("ee")) {
+                eeCount++;
+            } else if (sub.equals("eb")) {
+                ebCount++;
+            }
+        }
+
+        System.out.println(eeCount + " " + ebCount);
+
+        br.close();
     }
 }
