c731bbdbbe2089ba3d66abe998ccb77b6f9d8765
Sehui Jung <116075689+jungsehui@users.noreply.github.com>
2025-11-03 14:11:48 +0900

    Title: 첫 번째와 두 번째 교환, Time: 85ms, Memory: 9MB, Status: Passed - Codetree

--- 251103/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
+++ 251103/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -2,7 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
+
+    private static long N;
+    private static int M;
+    private static int[] playTimes;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        String line = br.readLine();
+        char firstChar = line.charAt(0);
+        char secondChar = line.charAt(1);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (c == firstChar) {
+                sb.append(secondChar);
+            } else if (c == secondChar) {
+                sb.append(firstChar);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        System.out.println(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
